a9b3f9a65223b85bcc7b0719586ac71651ba3dc8
Andrian Smetaniuk <andrian.smet@gmail.com>
2025-05-18 15:22:44 +0300

    update: add logo in header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link"
-
-import { Switch } from "../ui/switch"
+import Image from "next/image"
 
 export const Header = () => {
   return (
     <header className="flex items-center justify-between">
-      <Link href="/" className="text-[24px] font-medium text-black">CoinPulse</Link>
-      <Switch id="theme-mode" />
+      <Link href="/" className="relative w-30 h-10">
+        <Image
+          src="/images/logo.png"
+          alt="Logo"
+          fill
+          className="object-contain"
+          priority
+        />
+      </Link>
     </header>
   )
 }
